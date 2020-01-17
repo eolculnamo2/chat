806f1179125cf84137be16c68f159f51392b25aa
Rob Bertram <rbertram8@gmail.com>
2020-01-17 00:47:26 -0600

    init

---
+++ src/main/java/com/challenge/chatapp/resources/SocketController.java
@@ -0,0 +1,18 @@
+package com.challenge.chatapp.resources;
+
+import com.challenge.chatapp.dto.Message;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class SocketController {
+
+  @MessageMapping("/send-msg")
+  @SendTo("/topic/emit-msg")
+  public Message greeting(Message message) throws Exception {
+    return new Message(message.getUsername(), message.getMessage());
+  }
+
+}
